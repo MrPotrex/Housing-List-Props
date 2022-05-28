58c9d1e55db97f3bba03903da0acdf32ff5b4d3e
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-28 18:50:34 +0200

    Update propsES.lua

--- propsES.lua
+++ propsES.lua
@@ -1,1 +1,60 @@
--- LISTA EN ESPAÑOL
+-- LISTA DE PROPS EN ESPAÑOL
+-- NOMBRE DE LOS PROPS SACADOS DE AQUÍ: https://gtahash.ru/
+
+-- SI USAS UN SISTEMA DE CASAS MIRAD BIEN ANTES DE PONER ESTA LISTA.
+
+-- CATEGORIAS
+  {'chair', 'Sillas'},
+  {'table', 'Mesas'},
+  {'decorations', 'Decoraciones'},
+  {'electronics', 'Electronica'},
+  {'Plants', 'Plantas'},
+  {'sofa', 'Sofas'},
+  {'bed', 'Camas'},
+  {'jewels', 'Joyas'},
+  {'drugs', 'Drogas'},
+  {'food','Comida'},
+  {'lights', 'Luces'},
+  {'kitchen', 'Cocina'},
+  {'accessory', 'Accesorios'},
+  {'storage', 'Almacen'},
+
+-- SILLAS
+  {'Silla', 'prop_chair_02'}, -- Colision
+  {'Silla 2', 'prop_chair_01b'}, -- Colision
+  {'Silla 3', 'prop_chair_04a'}, -- Colision
+  {'Silla 4', 'prop_chair_04b'}, -- Colision
+  {'Silla 5', 'prop_chair_06'}, -- Colision
+  {'Silla 6', 'prop_chair_09'}, -- Colision
+  {'Silla 7', 'prop_chateau_chair_01'}, -- Colision
+  {'Silla 8', 'prop_direct_chair_01'}, -- Colision
+  {'Silla 9', 'prop_gc_chair02'}, -- Colision
+  {'Silla 10', 'prop_ld_farm_chair01'}, -- Colision
+  {'Silla 11', 'v_ilev_chair02_ped'}, -- Colision
+  {'Silla 12', 'v_corp_cd_chair'}, -- Colision
+  {'Silla 13', 'v_corp_bk_chair3'}, -- Colision
+  {'Silla 14', 'prop_skid_chair_03'}, -- Colision
+  {'Silla 15', 'prop_table_03_chr'}, -- Colision
+  {'Silla 26', 'prop_table_03b_chr'}, -- Colision
+  {'Silla 17', 'prop_table_04_chr'}, -- Colision
+  {'Silla 18', 'prop_table_05_chr'}, -- Colision
+  {'Silla 19', 'p_dinechair_01_s'}, -- Colision
+  {'Silla 20', 'prop_sol_chair'}, -- Colision
+  {'Silla 21', 'prop_table_02_chr'}, -- Colision
+  {'Silla 22', 'prop_rock_chair_01'}, -- Colision
+  {'Silla 23', 'prop_torture_ch_01'}, -- Colision
+  {'Silla 24', 'prop_chair_07'}, -- Colision
+  {'Silla 25', 'prop_clown_chair'}, -- Colision
+  {'Silla 26', 'prop_table_01_chr_a'}, -- Colision
+  {'Silla 27', 'prop_table_01_chr_b'}, -- Colision
+  {'Silla 28', 'p_clb_officechair_s'}, -- Colision
+  {'Silla 29', 'prop_off_chair_04_s'}, -- Colision
+  {'Silla 30', 'prop_cs_office_chair'}, -- Colision
+  {'Silla 31', 'prop_off_chair_04'}, -- Colision
+  {'Silla 32', 'prop_off_chair_03'}, -- Colision
+  {'Silla 33', 'prop_off_chair_01'}, -- Colision
+  {'Silla 34', 'prop_off_chair_05'}, -- Colision
+  {'Silla 35', 'v_club_officechair'}, -- Colision
+  {'Silla 36', 'v_corp_offchair'}, -- Colision
+
+
