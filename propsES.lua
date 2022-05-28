b05f01a7a09a798d0d60cf971e266f3e4271dfc3
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-28 22:06:53 +0200

    Añadido precio base ($50)
    Esto te servirá para poner el precio en los sistemas de casas tipo Loaf_Housing, etc...

--- propsES.lua
+++ propsES.lua
@@ -17,89 +17,89 @@
   {'lights', 'Luces'},
   {'kitchen', 'Cocina'},
   {'accessory', 'Accesorios'},
-  {'arcade', 'Arcade'},
+--  {'arcade', 'Arcade'},
   {'storage', 'Almacen'},
 
 -- SILLAS
-  {'Silla 1', 'prop_chair_02'}, -- Colision
-  {'Silla 2', 'prop_chair_01b'}, -- Colision
-  {'Silla 3', 'prop_chair_04a'}, -- Colision
-  {'Silla 4', 'prop_chair_04b'}, -- Colision
-  {'Silla 5', 'prop_chair_06'}, -- Colision
-  {'Silla 6', 'prop_chair_09'}, -- Colision
-  {'Silla 7', 'prop_chateau_chair_01'}, -- Colision
-  {'Silla 8', 'prop_direct_chair_01'}, -- Colision
-  {'Silla 9', 'prop_gc_chair02'}, -- Colision
+  {'Silla 1', 'prop_chair_02', 50}, -- Colision
+  {'Silla 2', 'prop_chair_01b', 50}, -- Colision
+  {'Silla 3', 'prop_chair_04a', 50}, -- Colision
+  {'Silla 4', 'prop_chair_04b', 50}, -- Colision
+  {'Silla 5', 'prop_chair_06', 50}, -- Colision
+  {'Silla 6', 'prop_chair_09', 50}, -- Colision
+  {'Silla 7', 'prop_chateau_chair_01', 50}, -- Colision
+  {'Silla 8', 'prop_direct_chair_01', 50}, -- Colision
+  {'Silla 9', 'prop_gc_chair02', 50}, -- Colision
 
-  {'Silla 10', 'prop_ld_farm_chair01'}, -- Colision
-  {'Silla 11', 'v_ilev_chair02_ped'}, -- Colision
-  {'Silla 12', 'v_corp_cd_chair'}, -- Colision
-  {'Silla 13', 'v_corp_bk_chair3'}, -- Colision
-  {'Silla 14', 'prop_skid_chair_03'}, -- Colision
-  {'Silla 15', 'prop_table_03_chr'}, -- Colision
-  {'Silla 26', 'prop_table_03b_chr'}, -- Colision
-  {'Silla 17', 'prop_table_04_chr'}, -- Colision
-  {'Silla 18', 'prop_table_05_chr'}, -- Colision
-  {'Silla 19', 'p_dinechair_01_s'}, -- Colision
+  {'Silla 10', 'prop_ld_farm_chair01', 50}, -- Colision
+  {'Silla 11', 'v_ilev_chair02_ped', 50}, -- Colision
+  {'Silla 12', 'v_corp_cd_chair', 50}, -- Colision
+  {'Silla 13', 'v_corp_bk_chair3', 50}, -- Colision
+  {'Silla 14', 'prop_skid_chair_03', 50}, -- Colision
+  {'Silla 15', 'prop_table_03_chr', 50}, -- Colision
+  {'Silla 26', 'prop_table_03b_chr', 50}, -- Colision
+  {'Silla 17', 'prop_table_04_chr', 50}, -- Colision
+  {'Silla 18', 'prop_table_05_chr', 50}, -- Colision
+  {'Silla 19', 'p_dinechair_01_s', 50}, -- Colision
 
-  {'Silla 20', 'prop_sol_chair'}, -- Colision
-  {'Silla 21', 'prop_table_02_chr'}, -- Colision
-  {'Silla 22', 'prop_rock_chair_01'}, -- Colision
-  {'Silla 23', 'prop_torture_ch_01'}, -- Colision
-  {'Silla 24', 'prop_chair_07'}, -- Colision
-  {'Silla 25', 'prop_clown_chair'}, -- Colision
-  {'Silla 26', 'prop_table_01_chr_a'}, -- Colision
-  {'Silla 27', 'prop_table_01_chr_b'}, -- Colision
-  {'Silla 28', 'p_clb_officechair_s'}, -- Colision
-  {'Silla 29', 'prop_off_chair_04_s'}, -- Colision
+  {'Silla 20', 'prop_sol_chair', 50}, -- Colision
+  {'Silla 21', 'prop_table_02_chr', 50}, -- Colision
+  {'Silla 22', 'prop_rock_chair_01', 50}, -- Colision
+  {'Silla 23', 'prop_torture_ch_01', 50}, -- Colision
+  {'Silla 24', 'prop_chair_07', 50}, -- Colision
+  {'Silla 25', 'prop_clown_chair', 50}, -- Colision
+  {'Silla 26', 'prop_table_01_chr_a', 50}, -- Colision
+  {'Silla 27', 'prop_table_01_chr_b', 50}, -- Colision
+  {'Silla 28', 'p_clb_officechair_s', 50}, -- Colision
+  {'Silla 29', 'prop_off_chair_04_s', 50}, -- Colision
 
-  {'Silla 30', 'prop_cs_office_chair'}, -- Colision
-  {'Silla 31', 'prop_off_chair_04'}, -- Colision
-  {'Silla 32', 'prop_off_chair_03'}, -- Colision
-  {'Silla 33', 'prop_off_chair_01'}, -- Colision
-  {'Silla 34', 'prop_off_chair_05'}, -- Colision
-  {'Silla 35', 'v_club_officechair'}, -- Colision
-  {'Silla 36', 'v_corp_offchair'}, -- Colision
-  {'Silla 37', 'prop_bar_stool_01'}, -- Colision
-  {'Silla 38', 'p_yacht_chair_01_s'}, -- Colision
-  {'Silla 39', 'prop_yacht_seat_03'}, -- Colision
+  {'Silla 30', 'prop_cs_office_chair', 50}, -- Colision
+  {'Silla 31', 'prop_off_chair_04', 50}, -- Colision
+  {'Silla 32', 'prop_off_chair_03', 50}, -- Colision
+  {'Silla 33', 'prop_off_chair_01', 50}, -- Colision
+  {'Silla 34', 'prop_off_chair_05', 50}, -- Colision
+  {'Silla 35', 'v_club_officechair', 50}, -- Colision
+  {'Silla 36', 'v_corp_offchair', 50}, -- Colision
+  {'Silla 37', 'prop_bar_stool_01', 50}, -- Colision
+  {'Silla 38', 'p_yacht_chair_01_s', 50}, -- Colision
+  {'Silla 39', 'prop_yacht_seat_03', 50}, -- Colision
 
-  {'Silla 40', 'prop_wheelchair_01'}, -- Colision
-  {'Silla 41', 'prop_armchair_01'}, -- Colision
-  {'Silla 42', 'prop_couch_sm_02'}, -- Colision
-  {'Silla 43', 'prop_couch_sm1_07'}, -- Colision
-  {'Silla 44', 'prop_yaught_chair_01'}, -- Colision
-  {'Silla 45', 'prop_couch_sm_05'}, -- Colision
-  {'Silla 46', 'prop_couch_sm2_07'}, -- Colision
-  {'Silla 47', 'prop_couch_sm_07'}, -- Colision
-  {'Silla 48', 'prop_chair_05'}, -- Colision
-  {'Silla 49', 'prop_table_06_chr'}, -- Colision
+  {'Silla 40', 'prop_wheelchair_01', 50}, -- Colision
+  {'Silla 41', 'prop_armchair_01', 50}, -- Colision
+  {'Silla 42', 'prop_couch_sm_02', 50}, -- Colision
+  {'Silla 43', 'prop_couch_sm1_07', 50}, -- Colision
+  {'Silla 44', 'prop_yaught_chair_01', 50}, -- Colision
+  {'Silla 45', 'prop_couch_sm_05', 50}, -- Colision
+  {'Silla 46', 'prop_couch_sm2_07', 50}, -- Colision
+  {'Silla 47', 'prop_couch_sm_07', 50}, -- Colision
+  {'Silla 48', 'prop_chair_05', 50}, -- Colision
+  {'Silla 49', 'prop_table_06_chr', 50}, -- Colision
 
-  {'Silla 50', 'prop_stool_01'}, -- Colision
-  {'Silla 51', 'prop_chair_10'}, -- Colision
-  {'Silla 52', 'prop_old_wood_chair'}, -- Colision
-  {'Silla 53', 'prop_chair_03'}, -- Colision
-  {'Silla 54', 'v_ret_gc_chair03'}, -- Colision
-  {'Silla 55', 'prop_off_chair_04b'}, -- Colision
-  {'Silla 56', 'prop_waiting_seat_01'}, -- Colision
-  {'Silla 57', 'prop_skid_chair_01'}, -- Colision
-  {'Silla 58', 'prop_skid_chair_02'}, -- Colision
-  {'Silla 59', 'prop_rub_couch03'}, -- Colision
+  {'Silla 50', 'prop_stool_01', 50}, -- Colision
+  {'Silla 51', 'prop_chair_10', 50}, -- Colision
+  {'Silla 52', 'prop_old_wood_chair', 50}, -- Colision
+  {'Silla 53', 'prop_chair_03', 50}, -- Colision
+  {'Silla 54', 'v_ret_gc_chair03', 50}, -- Colision
+  {'Silla 55', 'prop_off_chair_04b', 50}, -- Colision
+  {'Silla 56', 'prop_waiting_seat_01', 50}, -- Colision
+  {'Silla 57', 'prop_skid_chair_01', 50}, -- Colision
+  {'Silla 58', 'prop_skid_chair_02', 50}, -- Colision
+  {'Silla 59', 'prop_rub_couch03', 50}, -- Colision
 
-  {'Silla 60', 'hei_prop_yah_seat_01'}, -- Colision
-  {'Silla 61', 'hei_prop_yah_seat_02'}, -- Colision
-  {'Silla 62', 'p_armchair_01_s'}, -- Colision
-  {'Silla 63', 'p_ilev_p_easychair_s'}, -- Colision
-  {'Silla 64 (ATRAVIESAS)', 'p_soloffchair_s'}, -- NOT Colision
-  {'Silla 65', 'prop_wheelchair_01_s'}, -- Colision
-  {'Silla 66', 'v_ilev_fh_dineeamesa'}, -- Colision
-  {'Silla 67', 'v_ilev_fh_kitchenstool'}, -- Colision
-  {'Silla 68', 'v_ilev_leath_chr'}, -- Colision
-  {'Silla 69', 'v_ilev_hd_chair'}, -- Colision
+  {'Silla 60', 'hei_prop_yah_seat_01', 50}, -- Colision
+  {'Silla 61', 'hei_prop_yah_seat_02', 50}, -- Colision
+  {'Silla 62', 'p_armchair_01_s', 50}, -- Colision
+  {'Silla 63', 'p_ilev_p_easychair_s', 50}, -- Colision
+  {'Silla 64 (ATRAVIESAS)', 'p_soloffchair_s', 50}, -- NOT Colision
+  {'Silla 65', 'prop_wheelchair_01_s', 50}, -- Colision
+  {'Silla 66', 'v_ilev_fh_dineeamesa', 50}, -- Colision
+  {'Silla 67', 'v_ilev_fh_kitchenstool', 50}, -- Colision
+  {'Silla 68', 'v_ilev_leath_chr', 50}, -- Colision
+  {'Silla 69', 'v_ilev_hd_chair', 50}, -- Colision
 
-  {'Silla 70', 'v_ilev_m_dinechair'}, -- Colision
-  {'Silla 71', 'v_ilev_p_easychair'}, -- Colision
-  {'Silla 72', 'v_ilev_tort_stool'}, -- Colision
+  {'Silla 70', 'v_ilev_m_dinechair', 50}, -- Colision
+  {'Silla 71', 'v_ilev_p_easychair', 50}, -- Colision
+  {'Silla 72', 'v_ilev_tort_stool', 50}, -- Colision
 
 -- ARCADE: https://forum.cfx.re/t/mlo-cyber-bar-v2/4769478
 --  {'Bebida 1', 'boisson_hulk', 50},
