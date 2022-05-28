0343bbc5f3bd9cf9227ea651cb162299cb60cc66
Mancotrex <32984490+MrPotrex@users.noreply.github.com>
2022-05-28 20:09:28 +0200

    Update propsES.lua

--- propsES.lua
+++ propsES.lua
@@ -20,7 +20,7 @@
   {'storage', 'Almacen'},
 
 -- SILLAS
-  {'Silla', 'prop_chair_02'}, -- Colision
+  {'Silla 1', 'prop_chair_02'}, -- Colision
   {'Silla 2', 'prop_chair_01b'}, -- Colision
   {'Silla 3', 'prop_chair_04a'}, -- Colision
   {'Silla 4', 'prop_chair_04b'}, -- Colision
@@ -29,6 +29,7 @@
   {'Silla 7', 'prop_chateau_chair_01'}, -- Colision
   {'Silla 8', 'prop_direct_chair_01'}, -- Colision
   {'Silla 9', 'prop_gc_chair02'}, -- Colision
+
   {'Silla 10', 'prop_ld_farm_chair01'}, -- Colision
   {'Silla 11', 'v_ilev_chair02_ped'}, -- Colision
   {'Silla 12', 'v_corp_cd_chair'}, -- Colision
@@ -39,6 +40,7 @@
   {'Silla 17', 'prop_table_04_chr'}, -- Colision
   {'Silla 18', 'prop_table_05_chr'}, -- Colision
   {'Silla 19', 'p_dinechair_01_s'}, -- Colision
+
   {'Silla 20', 'prop_sol_chair'}, -- Colision
   {'Silla 21', 'prop_table_02_chr'}, -- Colision
   {'Silla 22', 'prop_rock_chair_01'}, -- Colision
@@ -49,6 +51,7 @@
   {'Silla 27', 'prop_table_01_chr_b'}, -- Colision
   {'Silla 28', 'p_clb_officechair_s'}, -- Colision
   {'Silla 29', 'prop_off_chair_04_s'}, -- Colision
+
   {'Silla 30', 'prop_cs_office_chair'}, -- Colision
   {'Silla 31', 'prop_off_chair_04'}, -- Colision
   {'Silla 32', 'prop_off_chair_03'}, -- Colision
@@ -58,5 +61,27 @@
   {'Silla 36', 'v_corp_offchair'}, -- Colision
   {'Silla 37', 'prop_bar_stool_01'}, -- Colision
   {'Silla 38', 'p_yacht_chair_01_s'}, -- Colision
+  {'Silla 39', 'prop_yacht_seat_03'}, -- Colision
 
+  {'Silla 40', 'prop_wheelchair_01'}, -- Colision
+  {'Silla 41', 'prop_armchair_01'}, -- Colision
+  {'Silla 42', 'prop_couch_sm_02'}, -- Colision
+  {'Silla 43', 'prop_couch_sm1_07'}, -- Colision
+  {'Silla 44', 'prop_yaught_chair_01'}, -- Colision
+  {'Silla 45', 'prop_couch_sm_05'}, -- Colision
+  {'Silla 46', 'prop_couch_sm2_07'}, -- Colision
+  {'Silla 47', 'prop_couch_sm_07'}, -- Colision
+  {'Silla 48', 'prop_chair_05'}, -- Colision
+  {'Silla 49', 'prop_table_06_chr'}, -- Colision
+
+  {'Silla 50', 'prop_stool_01'}, -- Colision
+  {'Silla 51', 'prop_chair_10'}, -- Colision
+  {'Silla 52', 'prop_old_wood_chair'}, -- Colision
+  {'Silla 53', 'prop_chair_03'}, -- Colision
+  {'Silla 54', 'v_ret_gc_chair03', 50}, -- Colision
+  {'Silla 55', 'prop_off_chair_04b', 50}, -- Colision
+  {'Silla 56', 'prop_waiting_seat_01', 50}, -- Colision
+  {'Silla 57', 'prop_skid_chair_01', 50}, -- Colision
+  {'Silla 58', 'prop_skid_chair_02', 50}, -- Colision
+  {'Silla 59', 'prop_rub_couch03', 50}, -- Colision
 
